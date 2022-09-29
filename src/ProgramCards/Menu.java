8f3860fd6217b5a3df95c2642e5501319b5b3bc0
Amitaf134 <113261500+Amitaf134@users.noreply.github.com>
2022-09-29 11:35:57 -0400

    Add files via upload
    Jeito arcaico de dar commit

---
+++ src/ProgramCards/Menu.java
@@ -0,0 +1,122 @@
+package ProgramCards;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class Menu extends JFrame{
+	 
+   	private static final long serialVersionUID = 1L;
+	    JButton ilha1,ilha2,ilha3,livre;
+    	ImageIcon image = new ImageIcon("F:/ilha1.png");
+    	ImageIcon image2 = new ImageIcon("F:/ilha2.png");
+	    JMenuBar barrinha;
+	    JMenu opcao;
+	    JMenuItem config, sair;
+	    Dimension dimensao = Toolkit.getDefaultToolkit().getScreenSize();
+        Color ouro = new Color(238,201,0);
+        Color ouroclaro = new Color(238,232,170);
+        Color azul = new Color(25,25,112);
+
+	  public Menu() {
+		
+	        setTitle("ILHAS");
+		  barrinha = new JMenuBar();
+		  
+		  setJMenuBar(barrinha);
+		  
+		  opcao = new JMenu("Opções");
+		
+		  config = new JMenuItem("Configurações");
+		  sair = new JMenuItem("sair");
+		  
+		  barrinha.add(opcao);
+		  barrinha.setBackground(ouro);
+		  
+		
+		  opcao.add(config);
+		  opcao.add(sair);	
+		  opcao.setBackground(ouro);
+		  
+		  
+		  config.setBackground(ouroclaro);
+		  config.setForeground(Color.black);
+		  
+		  sair.setBackground(ouroclaro);
+		  sair.setForeground(Color.black);
+	   
+		ilha1 = new JButton(image);
+		ilha1. setOpaque(false);
+                ilha1.setFocusable(false);
+                ilha1.setContentAreaFilled(false);
+                ilha1.setBorderPainted(false);
+		
+                ilha2 = new JButton("MINI_BOI");
+                ilha2 = new JButton(image2);
+		ilha2. setOpaque(false);
+                ilha2.setFocusable(false);
+                ilha2.setContentAreaFilled(false);
+                ilha2.setBorderPainted(false);
+        
+        
+		ilha2.setBackground(Color.WHITE);
+		ilha2.setForeground(Color.BLACK);
+
+                ilha3 = new JButton("MINI_BOI");
+		ilha3.setBackground(Color.WHITE);
+		ilha3.setForeground(Color.BLACK);      
+     
+                livre = new JButton("MINI_BOI");
+		livre.setBackground(Color.WHITE);
+		livre.setForeground(Color.BLACK);
+                   
+		AcionaBotao c = new AcionaBotao();
+		 ilha1.addActionListener(c);
+		 ilha2.addActionListener(c);
+                 ilha3.addActionListener(c);
+                 livre.addActionListener(c);
+
+	    Container cont = getContentPane();
+		cont.setBackground(azul);
+	   	cont.add(ilha1);
+                cont.add(ilha2);
+                cont.add(ilha3);
+                cont.add(livre);
+		
+		setLayout(null);
+		ilha1.setBounds(300,300,300,300);
+		ilha2.setBounds(700,300,300,300);
+		ilha3.setBounds(1100,300,300,300);
+		livre.setBounds(1500,300,300,300);
+
+                setSize(dimensao);
+		setVisible(true);
+		
+	} 
+	    
+	 private class AcionaBotao implements ActionListener {
+
+         public void actionPerformed(ActionEvent e){
+        	 
+         Sons.main(null);	 
+       	 new TelaNiveis();
+       	 dispose();
+       	  
+   }
+   }
+	public static void main(String[]args) {
+		
+		Menu menu = new Menu();
+		menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
---
+++ src/ProgramCards/TelaEntrada.java
@@ -0,0 +1,76 @@
+package ProgramCards;
+
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JProgressBar;
+
+public class TelaEntrada extends JFrame {
+    private static final long serialVersionUID = 1L;
+        JProgressBar barra = new JProgressBar();
+        JLabel logo,nome;
+        ImageIcon ima;
+        Color azul = new Color(25,25,112);
+        Color azulc = new Color(65,105,225);
+        Color ouro = new Color(238,201,0);
+        Font font = new Font("Arial",Font.BOLD,70);
+        ImageIcon image = new ImageIcon("F:/Logo.png");
+        Dimension dimensao = Toolkit.getDefaultToolkit().getScreenSize();
+        
+        public TelaEntrada(){
+            
+            logo = new JLabel(image);
+            nome = new JLabel("ProgramCards");
+            nome.setForeground(ouro);
+            nome.setFont(font);
+
+            barra.setStringPainted(true);
+            barra.setValue(50);
+            barra.setMaximum(1000);
+            barra.setForeground(ouro);
+
+            Container cont = getContentPane();
+            cont.setBackground(azul);
+            
+            cont.add(nome);
+            cont.add(logo);
+         
+            cont.add(barra);
+
+            nome.setBounds(710,100,800,250);
+            logo.setBounds(770,250,350,350);
+            barra.setBounds(710,670,500,50);
+
+            new Temporizador().start();
+            
+            setLayout(null);
+            setVisible(true);
+            setSize(dimensao);
+}
+             public class Temporizador extends Thread {
+                  public void run(){
+                      while(barra.getValue()<1000){
+                 try{
+                     sleep(40);
+                     barra.setValue(barra.getValue()+10);
+                 }catch(InterruptedException e){
+                   e.printStackTrace();
+        }
+     }
+                  
+                  new TelaLogin();
+                 setVisible(false);
+                 Sons2.main(null);
+    }
+             }
+                  public static void main(String[] args) {
+                   TelaEntrada ex = new TelaEntrada();
+                   ex.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     }
+}
