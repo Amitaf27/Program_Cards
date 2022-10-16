e3adb1cfba3b20c0b8a1092e3b4e4b862bec13f6
Manfred-pc <112772463+Manfred-pc@users.noreply.github.com>
2022-10-16 17:30:08 -0400

    Musica.java
    16/10/22
    
    Atualização.

---
+++ ProgramCards/Musica.java
@@ -0,0 +1,35 @@
+package ProgramCards;
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
+
+public class Musica {
+
+   static Clip clipM;
+   public void setFile(String fileName){
+
+     try{
+
+       File musica = new File(fileName);
+       AudioInputStream sound = AudioSystem.getAudioInputStream(musica);
+       clipM = AudioSystem.getClip();
+       clipM.open(sound);
+       } catch (Exception e){
+
+    	   JOptionPane.showMessageDialog(null, "Arquivo da música não encontrado.");
+       }
+   }
+     public void play(){
+
+       clipM.setFramePosition(0);
+       clipM.start();      
+     }
+
+     public void stop(){
+
+       clipM.stop();
+       clipM.close();
+     }
+   }
