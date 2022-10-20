5c1553d84a6a8a6fa0097dded5d2981b1ce49916
aninhabacaninha <113071941+aninhabacaninha@users.noreply.github.com>
2022-10-20 19:39:12 -0300

    Update TelaLogin.java

--- ProgramCards/TelaLogin.java
+++ ProgramCards/TelaLogin.java
@@ -1,187 +1,191 @@
-import java.awt.Color; 
- import java.awt.Container; 
- import java.awt.Dimension; 
- import java.awt.Font; 
- import java.awt.Toolkit; 
- import java.awt.event.ActionEvent; 
- import java.awt.event.ActionListener; 
-  
- import javax.swing.BorderFactory; 
- import javax.swing.ImageIcon; 
- import javax.swing.JButton; 
- import javax.swing.JFrame; 
- import javax.swing.JLabel; 
- import javax.swing.JOptionPane; 
- import javax.swing.JPanel; 
- import javax.swing.JPasswordField; 
- import javax.swing.JTextField; 
- import javax.swing.border.Border; 
-  
- public class TelaLogin extends JFrame { 
-  
- private static final long serialVersionUID = 1L; 
- //declaracao de atributos,objetos e variaveis 
-           JButton botaoLogin, botao2; 
-           Font fonte1 = new Font("Arial", Font.BOLD, 14); 
-           Font fonte2 = new Font("Arial", Font.BOLD, 30); 
-           Border b = BorderFactory.createLineBorder(Color.black, 2); 
-           JLabel loginT, rotulo2, rotulo3,cadas,ima; 
-           JTextField text1; 
-           JPasswordField senha; 
-           JButton cadastrar;           
-           ImageIcon imagem1; 
-           JPanel painel = new JPanel(); 
-           Color azul = new Color(18,10,143); 
-           Color azulc = new Color(65,105,225); 
-           Color ouro = new Color(238,201,0); 
-           Dimension dimensao = Toolkit.getDefaultToolkit().getScreenSize();
-           SomEfeito ef = new SomEfeito();
-          Musica musica = new Musica();
-            
-           
-           public TelaLogin() { 
- 
-           ef.setFile(TelaEntrada.efeitoSom);
-             
-            imagem1 = new ImageIcon(getClass().getResource("btsair.png")); 
-            painel.setBackground(azulc); 
-            painel.setLayout(null);
-            loginT =  new JLabel("LOGIN"); 
-            loginT.setForeground(Color.WHITE);//mudar cor do texto da label 
-            loginT.setFont(new Font("Arial",Font.BOLD,30));//alterar fonte do label 
-            rotulo2 = new JLabel(); 
-            rotulo3 = new JLabel(); 
-            cadas  = new JLabel("Novo aqui?"); 
-            cadas.setForeground(Color.WHITE);//mudar cor do texto da label 
-            text1 = new JTextField("Email ou senha de UsuÃ¡rio"); 
-            senha = new JPasswordField(); 
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+
+import BancoDados.Usuario;
+import BancoDados.UsuarioBD;
+
+public class TelaLogin extends JFrame {
+
+private static final long serialVersionUID = 1L;
+//declaracao de atributos,objetos e variaveis
+          JButton botaoLogin, botao2;
+          Font fonte1 = new Font("Arial", Font.BOLD, 14);
+      	  Font fonte2 = new Font("Arial", Font.BOLD, 30);
+      	  Border b = BorderFactory.createLineBorder(Color.black, 2);
+          JLabel loginT, rotulo2, rotulo3,cadas,ima;
+          JTextField text1;
+          JPasswordField senha;
+          JButton cadastrar;          
+	      ImageIcon imagem1;
+	      JPanel painel = new JPanel();
+          Color azul = new Color(18,10,143);
+          Color azulc = new Color(65,105,225);
+          Color ouro = new Color(238,201,0);
+          Dimension dimensao = Toolkit.getDefaultToolkit().getScreenSize();
+          
          
-            AcionaBotao evento = new AcionaBotao(); 
-            
-            botaoLogin = new JButton("LOGIN"); 
-            botaoLogin.addActionListener(evento); 
-            botaoLogin.setBackground(Color.GREEN); 
-            
-            botao2 = new JButton(imagem1); 
-             
-            botao2.addActionListener(evento); 
-            AcionaSair sair1 = new AcionaSair(); 
-            botao2.addActionListener(sair1); 
-             
-            botao2. setOpaque(false); 
-            botao2.setFocusable(false); 
-            botao2.setContentAreaFilled(false); 
-            botao2.setBorderPainted(false); 
-            
-            AcionaCadastro cad = new AcionaCadastro(); 
-            cadastrar = new JButton("Cadastre-se"); 
-            cadastrar.setForeground(Color.GREEN);//MUDA A COR DO TEXT DO BOTAO 
-            
-            cadastrar.addActionListener(cad); 
-            
-            cadastrar. setOpaque(false); 
-            cadastrar.setFocusable(false); 
-            cadastrar.setContentAreaFilled(false); 
-            cadastrar.setBorderPainted(false); 
-            
-            cadastrar.setMnemonic('c'); 
-            
-//adicionando elementos no painel
-            painel.add(loginT);
-            painel.add(rotulo2);
-            painel.add(rotulo3);
-            painel.add(text1);
-            painel.add(senha);
-            Container cont = getContentPane(); 
-            cont.setBackground(azul); 
+          public TelaLogin() {
            
-            setLayout(null); 
-            
-            cont.add(botao2); 
-            
-         //   cont.add(loginT); 
-        
-  
-         //   cont.add(rotulo2); 
-         //   cont.add(rotulo3); 
+           imagem1 = new ImageIcon(getClass().getResource("btsair.png"));
+           painel.setBackground(azulc);
+           loginT =  new JLabel("LOGIN");
+           loginT.setForeground(Color.WHITE);//mudar cor do texto da label
+           loginT.setFont(new Font("Arial",Font.BOLD,30));//alterar fonte do label
+           rotulo2 = new JLabel();
+           rotulo3 = new JLabel();
+           cadas  = new JLabel("Novo aqui?");
+           cadas.setForeground(Color.WHITE);//mudar cor do texto da label
+           text1 = new JTextField("Email ou senha de Usuário");
+           senha = new JPasswordField();
+       
+           AcionaBotao evento = new AcionaBotao();
+          
+           botaoLogin = new JButton("LOGIN");
+           botaoLogin.addActionListener(evento);
+           botaoLogin.setBackground(Color.GREEN);
+          
+           botao2 = new JButton(imagem1);
            
-         //   cont.add(text1); 
-          //  cont.add(senha); 
-    
+           botao2.addActionListener(evento);
+           AcionaSair sair1 = new AcionaSair();
+           botao2.addActionListener(sair1);
            
-            cont.add(botaoLogin); 
-  
-            cont.add(cadas); 
-            cont.add(cadastrar); 
-            cont.add(painel); 
-           
-            botao2.setBounds(10,10, 100, 50); 
-           
-            loginT.setBounds(170, 50, 300, 25 ); 
-            rotulo2.setBounds(80, 150, 280, 25); 
-            rotulo3.setBounds(820, 310, 280, 25); 
-            text1.setBounds(80, 150, 280, 25); 
-            senha.setBounds(825, 310, 280,25); 
-           
-            botaoLogin.setBounds(825, 390, 280, 25); 
-  
-            cadas.setBounds(890,930,200,25); 
-            cadastrar.setBounds(890,930,200,25); 
-            painel.setBounds(458, 100, 450, 450); 
-            painel.setBorder(b); 
-  
-           
-            setSize(1366,768); 
-            setVisible(true); 
-           
-        } 
-           private class AcionaBotao implements ActionListener { 
+           botao2. setOpaque(false);
+           botao2.setFocusable(false);
+           botao2.setContentAreaFilled(false);
+           botao2.setBorderPainted(false);
+          
+           AcionaCadastro cad = new AcionaCadastro();
+           cadastrar = new JButton("Cadastre-se");
+           cadastrar.setForeground(Color.GREEN);//MUDA A COR DO TEXT DO BOTAO
+          
+           cadastrar.addActionListener(cad);
+          
+           cadastrar. setOpaque(false);
+           cadastrar.setFocusable(false);
+           cadastrar.setContentAreaFilled(false);
+           cadastrar.setBorderPainted(false);
+          
+           cadastrar.setMnemonic('c');
+          
+           Container cont = getContentPane();
+           cont.setBackground(azul);
+         
+           setLayout(null);
+          
+           cont.add(botao2);
+          
+           cont.add(loginT);
       
-              public void actionPerformed(ActionEvent e) { 
-        
-                    ef.play();
-                     String senha1 = new String(senha.getPassword()).trim(); 
+
+           cont.add(rotulo2);
+           cont.add(rotulo3);
          
-                   if (text1.getText().equalsIgnoreCase("mestreAndre") && senha1.equalsIgnoreCase("andre123")){ 
-              
-                       new Menu(); 
-                      dispose(); 
-                      setVisible(false); 
-                    }else { 
+           cont.add(text1);
+           cont.add(senha);
+  
+         
+           cont.add(botaoLogin);
+
+           cont.add(cadas);
+           cont.add(cadastrar);
+           cont.add(painel);
+         
+           botao2.setBounds(10,10, 100, 50);
+         
+           loginT.setBounds(920, 250, 300, 25 );
+           rotulo2.setBounds(820, 330, 280, 25);
+           rotulo3.setBounds(820, 360, 280, 25);
+           text1.setBounds(825, 330, 280, 25);
+           senha.setBounds(825, 360, 280,25);
+         
+           botaoLogin.setBounds(825, 390, 280, 25);
+
+           cadas.setBounds(890,930,200,25);
+           cadastrar.setBounds(890,930,200,25);
+           painel.setBounds(715, 150, 500, 500);
+           painel.setBorder(b);
+
+         
+           setSize(dimensao);
+           setVisible(true);
+         
+       }
+          private class AcionaBotao implements ActionListener {
+    
+             public void actionPerformed(ActionEvent e) {
+            	
+              try {
+            	String nomeUsuario = text1.getText();
+              	String senha1 = new String(senha.getPassword()).trim();
+              	
+              	Usuario usu = new Usuario();
+              	usu.setNomeUsuario(nomeUsuario);
+              	usu.setSenha(senha1);
+              	
+              	UsuarioBD usuBD = new UsuarioBD();
+              	ResultSet rsusuBD = usuBD.autenticUsu(usu);
+              	
+              	if (rsusuBD.next()) {
+              		Menu menu = new Menu();
+              		menu.setVisible(true);
+              		dispose();
+              	} else {
+              		JOptionPane.showMessageDialog(null, "Usuário ou senha inválidos" + e);
+              	}
+              	
+              } catch(SQLException err) {
+            	  JOptionPane.showMessageDialog(null, "TelaLogin" + e);
+              }
+            	
+                 //setVisible(false);
+             }
+
+
+           }
+             private class AcionaCadastro implements ActionListener {
+
+               public void actionPerformed(ActionEvent e){
+             	  new TelaCadastro();
+    	          setVisible(false);
+    	  
+              }
+            }
           
-                    JOptionPane.showMessageDialog(null,"Dados incorretos"); 
-    } 
-  
- } 
-  
-  
-            } 
-              private class AcionaCadastro implements ActionListener { 
-  
-                public void actionPerformed(ActionEvent e){
- 
-                        ef.play();
-                        new TelaCadastro(); 
-                       setVisible(false); 
-                
- } 
- } 
-           private class AcionaSair implements ActionListener { 
-  
-               public void actionPerformed(ActionEvent e){ 
-                        
-                      ef.play();
-                       System.exit(0); 
-                        
-         } 
-         } 
-  
-  
-              public static void main (String[]args){ 
-                       
-                      
-                 TelaLogin ex = new TelaLogin(); 
-                 ex.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-          
-         } 
- }
+            private class AcionaSair implements ActionListener {
+
+              public void actionPerformed(ActionEvent e){
+            	  
+            	 // Sons.main(null);
+            	  System.exit(0);
+            	  
+              }
+         }
+
+
+	     public static void main (String[]args){
+	    	 
+	    	
+        	TelaLogin ex = new TelaLogin();
+	        ex.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	}
+}
