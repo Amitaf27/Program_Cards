4ba5e717eceb194f7d8a0db8e677d3a014f1b572
aninhabacaninha <113071941+aninhabacaninha@users.noreply.github.com>
2022-10-20 17:09:22 -0300

    Update UsuarioBD.java

--- ProgramCards/BancoDados/UsuarioBD.java
+++ ProgramCards/BancoDados/UsuarioBD.java
@@ -2,13 +2,18 @@ package BancoDados;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
 
 public class UsuarioBD {
+	Connection connLogin;
 	
 	Connection conn;
 	PreparedStatement pstm;
+	
+	//Cadastro de Usuário
 	
 	public void cadastrar(Usuario usu) {
 		String sql = "insert into Usuario (nome, nomeUsuario, idade, senha) values (?, ?, ?, ?)";
@@ -25,8 +30,28 @@ public class UsuarioBD {
 			 pstm.execute();
 			 pstm.close();
 			
-		} catch (Exception e) {
+		} catch (SQLException e) {
 			JOptionPane.showMessageDialog(null, "UsuarioBD" + e);
+		}
+	}
+	
+	//Autenticação de Login
+	
+	public ResultSet autenticUsu(Usuario obj) {
+		connLogin = new ConexaoBD().conectaBD();
+		
+		try {
+			String sqlLogin = "select * from Usuario where nomeUsuario = ? and senha = ?";
+			PreparedStatement pstm2 = conn.prepareStatement(sqlLogin);
+			 pstm2.setString(1, obj.getNomeUsuario());
+			 pstm2.setString(2, obj.getSenha());
+			 
+			 ResultSet rs = pstm2.executeQuery();
+			 return rs;
+			 
+		} catch(SQLException e) {
+			JOptionPane.showMessageDialog(null, "Usuario" + e);
+			return null;
 		}
 	}
 	
