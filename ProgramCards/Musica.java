eb4545ae4c52b8fbe2c76504c7f39fe1ad00cdc7
Manfred-pc <112772463+Manfred-pc@users.noreply.github.com>
2022-10-31 10:31:00 -0400

    Update Musica.java

--- ProgramCards/Musica.java
+++ ProgramCards/Musica.java
@@ -6,6 +6,7 @@ import javax.swing.JOptionPane;
 public class Musica {
 
    static Clip clipM;
+   static boolean ligado = true;
    
      public void play(){
     	 
@@ -18,13 +19,28 @@ public class Musica {
 
     	    	   JOptionPane.showMessageDialog(null, "Arquivo da mÃƒÂºsica nÃƒÂ£o encontrado.");
     	       }
+    	   
+    	      if(ligado){
+    	   
+    	   
     	   	   clipM.setFramePosition(0);
-    	   	   clipM.start();      
+    	   	   clipM.start();
+    	      } else{
+    	    	  
+    	    	  
+    	    	  
+    	      }
      }
 
      public void stop(){
 
        clipM.stop();
        clipM.close();
+     }
+     
+     public void muda(){
+    	 
+    	 ligado = !ligado;
+    	 
      }
    }
