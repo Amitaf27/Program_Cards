53fcef40496c4f573d4194aaad5bdc7318fe3419
Amitaf134 <113261500+Amitaf134@users.noreply.github.com>
2022-11-03 12:36:33 -0400

    Add files via upload

--- ProgramCards/TelaNiveis.java
+++ ProgramCards/TelaNiveis.java
@@ -1,3 +1,20 @@
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.border.Border;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Container;
+
 public class TelaNiveis extends JFrame {
 	   JLabel rotulo1;
 	   JMenuBar barrinha;
