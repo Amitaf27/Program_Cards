61ce77cc6fc39015940726134515fb372e46aeec
aninhabacaninha <113071941+aninhabacaninha@users.noreply.github.com>
2022-11-13 19:20:35 -0300

    Update ProgramBD.sql

--- ProgramCards/BancoDados/ProgramBD.sql
+++ ProgramCards/BancoDados/ProgramBD.sql
@@ -4,10 +4,11 @@ use programbd;
 drop table if exists usuario;
 
 create table usuario (
-    nomeUsuario varchar(50) primary key unique,
+	nomeUsuario varchar(50) primary key unique,
     senha char(10) unique not null,
     nome varchar(50),
-    email varchar(100)
+    email varchar(100),
+    resultado_quest char(20) check(resultado_quest = "Iniciante" or resultado_quest = "Intermediário" or resultado_quest = "Avançado")
 );
 
 select * from usuario;
